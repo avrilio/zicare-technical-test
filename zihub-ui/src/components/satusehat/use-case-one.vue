8e6d034990537607b6247ffbdc5727a14a620869
avrilio <damar.maulanan@gmail.com>
2024-06-24 14:51:51 +0700

    menambahkan modal add data untuk use case 1

--- zihub-ui/src/components/satusehat/use-case-one.vue
+++ zihub-ui/src/components/satusehat/use-case-one.vue
@@ -116,7 +116,7 @@
         </div>
 
         <div v-if="isModalOpen" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-            <div class="bg-zihub-background p-8 rounded-lg shadow-lg w-1/2">
+            <div class="bg-zihub-background p-8 rounded-lg w-1/2">
                 <div class="flex justify-between items-center mb-4">
                     <div class="flex items-center space-x-2 text-white">
                         <font-awesome-icon icon="fa-solid fa-plus" />
@@ -145,7 +145,13 @@
                     </div>
                     <div>
                         <label for="modalPatientNik" class="table-filter-input-label">Patient NIK</label>
-                        <input type="text" id="modalPatientNik" class="table-filter-input mt-1 block w-full" />
+                        <div class="flex space-x-2">
+                            <input type="text" id="modalPatientNik" class="table-filter-input mt-1 block flex-grow" />
+                            <button class="bg-blue-700 text-white px-4 py-2 rounded-md mt-1 h-10">
+                                <font-awesome-icon icon="fa-solid fa-search" />
+                                <span class="ml-2">Search Data</span>
+                            </button>
+                        </div>
                     </div>
                     <div>
                         <label for="modalPractitionerSid" class="table-filter-input-label">Practitioner SID</label>
@@ -153,7 +159,7 @@
                     </div>
                     <div class="flex justify-end space-x-4">
                         <button @click="closeModal" class="bg-red-600 text-white px-4 py-2 rounded-md">Cancel</button>
-                        <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Save Data</button>
+                        <button class="bg-blue-700 text-white px-4 py-2 rounded-md">Save Data</button>
                     </div>
                 </div>
             </div>
