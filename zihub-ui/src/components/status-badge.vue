6923f373bfd48ffa1e79717576bfb63b82e1a2bd
avrilio <damar.maulanan@gmail.com>
2024-06-25 08:27:34 +0700

    membenarkan bug di status-badge.vue

--- zihub-ui/src/components/status-badge.vue
+++ zihub-ui/src/components/status-badge.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script setup>
+import { computed } from 'vue';
 
 const props = defineProps({
   status: {
@@ -13,12 +14,14 @@ const props = defineProps({
   }
 });
 
-const badgeClass = {
-  'text-xs font-semibold mr-2 px-2.5 py-0.5 rounded-2xl border-2 border-zihub-border bg-transparent': true,
-  'text-green-300': props.status === 'SUCCESS',
-  'text-red-300': props.status === 'FAILED',
-  'text-white': props.status === 'DRAFT'
-};
+const badgeClass = computed(() => {
+  return {
+    'text-xs font-semibold mr-2 px-2.5 py-0.5 rounded-2xl border-2 border-zihub-border bg-transparent': true,
+    'text-green-300': props.status === 'SUCCESS',
+    'text-red-300': props.status === 'FAILED',
+    'text-white': props.status === 'DRAFT'
+  };
+});
 
 </script>
 
